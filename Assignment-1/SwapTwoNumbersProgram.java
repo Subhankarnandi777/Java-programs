6e90924a0bb2aac5d7e5f8deb1fb0af304f439ac
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-20 08:55:53 +0530

    Create SwapTwoNumbersProgram.java

---
+++ Assignment-1/SwapTwoNumbersProgram.java
@@ -0,0 +1,11 @@
+class SwapTwoNumbersProgram {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After Swap: a = " + a + ", b = " + b);
+    }
+}
