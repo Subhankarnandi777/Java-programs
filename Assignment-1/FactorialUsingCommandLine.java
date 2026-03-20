8526db59c16bd1fd5ffdeef9c3b2314ba7bb5c73
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-20 08:54:42 +0530

    Create FactorialUsingCommandLine.java

---
+++ Assignment-1/FactorialUsingCommandLine.java
@@ -0,0 +1,12 @@
+class FactorialUsingCommandLine {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int fact = 1;
+
+        for(int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial = " + fact);
+    }
+}
