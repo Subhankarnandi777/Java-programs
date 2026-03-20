1e8674e06603d960d1d15f2b058cdbd46a1ad550
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-20 08:55:07 +0530

    Create OddEvenProgram.java

---
+++ Assignment-1/OddEvenProgram.java
@@ -0,0 +1,10 @@
+class OddEvenProgram {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if(num % 2 == 0)
+            System.out.println("Even Number");
+        else
+            System.out.println("Odd Number");
+    }
+}
