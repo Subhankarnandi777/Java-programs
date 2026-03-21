5e49d8072769f65ed62048fcee5046b88dc884b0
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-22 02:01:22 +0530

    Create BufferedReaderInputProgram.java

---
+++ Assignment -2/BufferedReaderInputProgram.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class BufferedReaderInputProgram {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter your name: ");
+        String name = br.readLine();
+
+        System.out.println("Hello " + name);
+    }
+}
