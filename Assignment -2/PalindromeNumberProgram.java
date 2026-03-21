fb6219baabaa8c5005e5b4bc136d24563d8a8574
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-22 02:03:10 +0530

    Create PalindromeNumberProgram.java

---
+++ Assignment -2/PalindromeNumberProgram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class PalindromeNumberProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+        int original = num;
+        int reverse = 0;
+
+        while(num != 0) {
+            reverse = reverse * 10 + (num % 10);
+            num /= 10;
+        }
+
+        if(original == reverse)
+            System.out.println("Palindrome Number");
+        else
+            System.out.println("Not a Palindrome");
+
+        sc.close();
+    }
+}
