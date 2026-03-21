e06f6c3660fb684e424f927d975f53ad838e1256
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-22 02:05:36 +0530

    Create StringMethodsDemoProgram.java

---
+++ Assignment -2/StringMethodsDemoProgram.java
@@ -0,0 +1,18 @@
+class StringMethodsDemoProgram {
+    public static void main(String[] args) {
+
+        String s = "Hello Java";
+
+        System.out.println("charAt: " + s.charAt(1));
+        System.out.println("compareTo: " + s.compareTo("Hello"));
+        System.out.println("equals: " + s.equals("Hello Java"));
+        System.out.println("equalsIgnoreCase: " + s.equalsIgnoreCase("hello java"));
+        System.out.println("indexOf: " + s.indexOf("Java"));
+        System.out.println("length: " + s.length());
+        System.out.println("substring: " + s.substring(0,5));
+        System.out.println("toLowerCase: " + s.toLowerCase());
+        System.out.println("toUpperCase: " + s.toUpperCase());
+        System.out.println("trim: " + "  Java  ".trim());
+        System.out.println("valueOf: " + String.valueOf(100));
+    }
+}
