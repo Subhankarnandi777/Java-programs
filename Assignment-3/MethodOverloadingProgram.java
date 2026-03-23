75b5c793bc2914da8726a33d55e56ea9e1b90b68
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-23 21:30:12 +0530

    Create MethodOverloadingProgram.java

---
+++ Assignment-3/MethodOverloadingProgram.java
@@ -0,0 +1,22 @@
+class MethodOverloadingProgram {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        MethodOverloadingProgram obj = new MethodOverloadingProgram();
+
+        System.out.println("Sum of 2 integers: " + obj.add(10, 20));
+        System.out.println("Sum of 3 integers: " + obj.add(10, 20, 30));
+        System.out.println("Sum of 2 doubles: " + obj.add(5.5, 4.5));
+    }
+}
