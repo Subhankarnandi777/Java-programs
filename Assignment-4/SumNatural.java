5a47c6b7227ae68b88b54896c884dd730a24b05a
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-26 10:36:03 +0530

    Create SumNatural.java

---
+++ Assignment-4/SumNatural.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class SumNatural {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter range: ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++)
+            sum += i;
+
+        System.out.println("Sum = " + sum);
+    }
+}
