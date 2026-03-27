20736044cc220678e1fefa8857a3436b8a99ffef
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-27 14:00:52 +0530

    Create StaticMethod.java

---
+++ Assignment-5/StaticMethod.java
@@ -0,0 +1,10 @@
+class StaticMethod {
+
+    static void display() {
+        System.out.println("Static Method Called");
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
