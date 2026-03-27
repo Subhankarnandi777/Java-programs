a59fcb191d3ecb731c8063bfda047fec685e6ae0
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-27 14:03:42 +0530

    Create MultiInheritJustify.java

---
+++ Assignment-5/MultiInheritJustify.java
@@ -0,0 +1,7 @@
+class MultiInheritJustify {
+    public static void main(String[] args) {
+        System.out.println("Java does not support multiple inheritance using classes");
+        System.out.println("Because it creates ambiguity (Diamond Problem)");
+        System.out.println("Java supports multiple inheritance using interfaces.");
+    }
+}
