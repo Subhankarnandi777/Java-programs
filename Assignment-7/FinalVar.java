0ac451cf5738da62b6957e1b26edde0d1bbc772a
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-29 23:05:30 +0530

    Add FinalVar class demonstrating final variable usage

---
+++ Assignment-7/FinalVar.java
@@ -0,0 +1,8 @@
+class FinalVar {
+    final int x = 10;
+
+    public static void main(String[] args) {
+        FinalVar obj = new FinalVar();
+        System.out.println("Final Variable = " + obj.x);
+    }
+}
