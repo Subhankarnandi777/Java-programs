5f66ac46039db0d7373cf30cdbfa352fc182cf3a
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-29 23:06:19 +0530

    Add FinalClass and FinalClassDemo with display method

---
+++ Assignment-7/FinalClassDemo.java
@@ -0,0 +1,12 @@
+final class FinalClass {
+    void display() {
+        System.out.println("Final Class Method");
+    }
+}
+
+class FinalClassDemo {
+    public static void main(String[] args) {
+        FinalClass obj = new FinalClass();
+        obj.display();
+    }
+}
