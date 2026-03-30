5c1654f9f8970104388752cf6a3eaed1a918cd6f
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-30 22:23:24 +0530

    Add ExCase2.java with exception handling example

---
+++ Assignment-8/ExCase2.java
@@ -0,0 +1,9 @@
+class ExCase2 {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Exception Handled");
+        }
+    }
+}
