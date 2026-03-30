f26264f4c68e70eec26c223a142bd6dd4bc4166a
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-03-30 22:22:28 +0530

    Create ExCase1.java with division by zero example
    Added ExCase1.java that demonstrates division by zero.

---
+++ Assignment-8/ExCase1.java
@@ -0,0 +1,8 @@
+class ExCase1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c = a / b;   // Exception occurs
+        System.out.println(c);
+    }
+}
