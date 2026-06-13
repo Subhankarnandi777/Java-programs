2a24d1b694e210fe263582e99100ccb3bc46a16a
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-06-13 13:39:37 +0530

    Add files via upload

---
+++ Sorting/Bubble_Sort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+// bubble sort (Time complexity of = O(n^2)) 
+
+import java.util.*;
+
+public class Bubble_Sort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");  
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 3, 1, 2};
+
+        // Bubble sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {  // use j, not i
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        printArray(arr);
+    }
+}
+
